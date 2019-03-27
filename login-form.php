eae62461faedc99bf961b347b2862e7abd3ea50e
Andrey Popov <andpop@mail.ru>
2019-03-27 07:01:48 +0300

    Begin authentication.

--- login-form.php
+++ login-form.php
@@ -26,7 +26,7 @@
         <input type="password" id="inputPassword" class="form-control" placeholder="Пароль" required name="password">
         <div class="checkbox mb-3">
           <label>
-            <input type="checkbox" value="remember-me"> Запомнить меня
+            <input type="checkbox" value="remember-me" name="remember-me"> Запомнить меня
           </label>
         </div>
         <button class="btn btn-lg btn-primary btn-block" type="submit">Войти</button>
